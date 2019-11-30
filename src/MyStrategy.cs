d557735c75427588d995957920f9110453bcb4a7
madddmax <m_prokofiev@mail.ru>
2019-11-30 17:19:50 +0300

    Removed shooting on the wall

--- src/MyStrategy.cs
+++ src/MyStrategy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using AiCup2019.Model;
 
@@ -5,26 +6,9 @@ namespace AiCup2019
 {
     public class MyStrategy
     {
-        static double DistanceSqr(Vec2Double a, Vec2Double b)
-        {
-            return (a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y);
-        }
         public UnitAction GetAction(Unit unit, Game game, Debug debug)
         {
             debug.Draw(new CustomData.Log("Version 2"));
-
-            Unit? nearestEnemy = null;
-            foreach (var other in game.Units)
-            {
-                if (other.PlayerId != unit.PlayerId)
-                {
-                    if (!nearestEnemy.HasValue || 
-                        DistanceSqr(unit.Position, other.Position) < DistanceSqr(unit.Position, nearestEnemy.Value.Position))
-                    {
-                        nearestEnemy = other;
-                    }
-                }
-            }
 
             LootBox? nearestWeapon = null;
             var hasLight = game.LootBoxes.Any(l => l.Item is Item.Weapon w && w.WeaponType != WeaponType.RocketLauncher);
@@ -45,20 +29,36 @@ namespace AiCup2019
                 }
             }
 
+            Unit? nearestEnemy = null;
+            foreach (var other in game.Units)
+            {
+                if (other.PlayerId == unit.PlayerId)
+                {
+                    continue;
+                }
+
+                if (!nearestEnemy.HasValue ||
+                    DistanceSqr(unit.Position, other.Position) < DistanceSqr(unit.Position, nearestEnemy.Value.Position))
+                {
+                    nearestEnemy = other;
+                }
+            }
+
+            if (nearestEnemy == null)
+            {
+                return EmptyAction();
+            }
+
+            var enemy = nearestEnemy.Value;
+
             Vec2Double targetPos = unit.Position;
             if (!unit.Weapon.HasValue && nearestWeapon.HasValue)
             {
                 targetPos = nearestWeapon.Value.Position;
             }
-            else if (nearestEnemy.HasValue)
+            else
             {
-                targetPos = nearestEnemy.Value.Position;
-            }
-
-            Vec2Double aim = new Vec2Double(0, 0);
-            if (nearestEnemy.HasValue)
-            {
-                aim = new Vec2Double(nearestEnemy.Value.Position.X - unit.Position.X, nearestEnemy.Value.Position.Y - unit.Position.Y);
+                targetPos = enemy.Position;
             }
 
             bool jump = targetPos.Y > unit.Position.Y;
@@ -79,11 +79,40 @@ namespace AiCup2019
 
             action.Jump = jump;
             action.JumpDown = !jump;
-            action.Aim = aim;
+            action.Aim = new Vec2Double(enemy.Position.X - unit.Position.X, enemy.Position.Y - unit.Position.Y); ;
 
-            action.Shoot = nearestEnemy.HasValue && 
-                           unit.Weapon.HasValue &&
-                           DistanceSqr(unit.Position, nearestEnemy.Value.Position) < 10;
+            action.Shoot = false;
+            if (unit.Weapon.HasValue)
+            {
+                action.Shoot = true;
+                if (Math.Abs(unit.Position.X - enemy.Position.X) >
+                    Math.Abs(unit.Position.Y - enemy.Position.Y))
+                {
+                    double startX = unit.Position.X < enemy.Position.X ? unit.Position.X : enemy.Position.X;
+                    double endX = unit.Position.X < enemy.Position.X ? enemy.Position.X : unit.Position.X;
+                    for (double x = startX + 0.5; x < endX; x += 0.5)
+                    {
+                        var y = Y(unit.Position, enemy.Position, x);
+                        if (game.Level.Tiles[(int) Math.Floor(x)][(int) Math.Floor(y)] == Tile.Wall)
+                        {
+                            action.Shoot = false;
+                        }
+                    }
+                }
+                else
+                {
+                    double startY = unit.Position.Y < enemy.Position.Y ? unit.Position.Y : enemy.Position.Y;
+                    double endY = unit.Position.Y < enemy.Position.Y ? enemy.Position.Y : unit.Position.Y;
+                    for (double y = startY + 0.5; y < endY; y += 0.5)
+                    {
+                        var x = X(unit.Position, enemy.Position, y);
+                        if (game.Level.Tiles[(int)Math.Floor(x)][(int)Math.Floor(y)] == Tile.Wall)
+                        {
+                            action.Shoot = false;
+                        }
+                    }
+                }
+            }
 
             action.SwapWeapon = nearestWeapon.HasValue &&
                                 unit.Weapon.HasValue &&
@@ -94,6 +123,35 @@ namespace AiCup2019
             action.PlantMine = false;
 
             return action;
+        }
+
+        private static UnitAction EmptyAction()
+        {
+            return new UnitAction
+            {
+                Velocity = 0,
+                Aim = new Vec2Double(0, 0),
+                Jump = false,
+                JumpDown = false,
+                PlantMine = false,
+                Shoot = false,
+                SwapWeapon = false
+            };
+        }
+
+        private static double DistanceSqr(Vec2Double a, Vec2Double b)
+        {
+            return (a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y);
+        }
+
+        private static double X(Vec2Double p1, Vec2Double p2, double y)
+        {
+            return (y - p1.Y) * (p2.X - p1.X) / (p2.Y - p1.Y) + p1.X;
+        }
+
+        private static double Y(Vec2Double p1, Vec2Double p2, double x)
+        {
+            return (x - p1.X) * (p2.Y - p1.Y) / (p2.X - p1.X) + p1.Y;
         }
     }
 }
